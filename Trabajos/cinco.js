bce398b913af86b5ea5fec0bc4491bf307540995
Fernanda López <fer.daye.2007@gmail.com>
2024-04-14 18:58:08 -0600

    cinco ajsutado

---
+++ Trabajos/cinco.js
@@ -0,0 +1,13 @@
+// ingresa un numero aqui 
+//           🔰
+let numero1 = 5
+// ingresa un segundo numero aqui 
+//           🔰
+let numero2 = 8
+
+if (numero1 > numero2){
+    console.log(numero1  + " es mayor que ", numero2);
+}else{
+    console.log(numero2 + " es mayor que ", numero1);
+}
+
