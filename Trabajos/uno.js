506cac6915e675bdd0a88a1959ded13ddc3537a2
Fernanda López <fer.daye.2007@gmail.com>
2024-04-14 18:37:37 -0600

    uno

---
+++ Trabajos/uno.js
@@ -0,0 +1,19 @@
+// eres bonit@?
+// pon tu respuesta 
+let respondes = true 
+
+
+const eresBonita = new Promise((resolve, reject ) => {
+    if (respondes) {
+        resolve("obvio que siii")
+    } else {
+        reject("no te creo")
+    }
+})
+
+eresBonita
+.then((response)=>{
+    console.log(response);
+}).catch((error)=>{
+    console.error(error);
+})
