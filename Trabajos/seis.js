25dd982551e8719e93de22ab4daf5b7331cac4da
Fernanda López <fer.daye.2007@gmail.com>
2024-04-15 13:00:10 -0600

    seis

--- Trabajos/seis.js
+++ Trabajos/seis.js
@@ -1,8 +1,16 @@
-let numeros = [15, 5, 15];
-let 
-valores.forEach( valores=>{
-    suma+=valores
-    })
-    console.log(suma);
-    
+const numeros = [5, 5, 8]; 
+let maximo = numeros[0]; 
+let numerosIguales = [];
 
+numeros.forEach(numero => {
+    if (numero > maximo) {
+        maximo = numero;
+    } else if (numero === maximo && !numerosIguales.includes(numero)) {
+        numerosIguales.push(numero); 
+    }
+});
+
+console.log("El número más grande es:", maximo);
+if (numerosIguales.length > 0) {
+    console.log("Los números iguales son:", numerosIguales.join(", "));
+}
